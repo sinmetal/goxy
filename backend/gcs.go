dde55efc6c4cb91c0f25b53e733559fd119c8f50
sinmetal <metal.tie@gmail.com>
2019-12-30 16:29:13 +0900

    migration 2nd gen refs #3

--- backend/gcs.go
+++ backend/gcs.go
@@ -1,29 +1,23 @@
 package backend
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
 
 	"cloud.google.com/go/storage"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
 )
 
-func init() {
-	http.HandleFunc("/gcs/", handlerGCS)
-}
+func HandlerGCS(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
 
-func handlerGCS(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
-
-	log.Infof(ctx, "Handler GCS Path : %s", r.URL.Path)
 	paths := strings.Split(r.URL.Path, "/")
 
-	log.Infof(ctx, "%+v", paths)
 	bucketName := paths[2]
 	objectName := strings.Join(paths[3:], "/")
-	log.Infof(ctx, "Bucket:%s, Object:%s", bucketName, objectName)
+	fmt.Printf("Bucket:%s, Object:%s\n", bucketName, objectName)
 
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -45,7 +39,11 @@ func handlerGCS(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer reader.Close()
+	defer func() {
+		if err := reader.Close(); err != nil {
+			fmt.Printf("failed gcs reader close. err=%+v\n", err)
+		}
+	}()
 
 	attrs, err := bkt.Object(objectName).Attrs(ctx)
 	if err != nil {
@@ -53,13 +51,12 @@ func handlerGCS(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Infof(ctx, "Content-Type:%s", attrs.ContentType)
-
 	w.Header().Set("Content-Type", attrs.ContentType)
+	w.Header().Set("Cache-Control", "public, max-age=600")
 	w.WriteHeader(http.StatusOK)
 	_, err = io.Copy(w, reader)
 	if err != nil {
-		log.Errorf(ctx, "response copy error. err = %+v", err)
+		fmt.Printf("response copy error. err = %+v\n", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sinmetal/goxy
+
+go 1.12
+
+require (
+	cloud.google.com/go/storage v1.4.0
+	google.golang.org/appengine v1.6.5
+)
